44f2393d7a3f0088ea60c004851dd2a5cd1149c2
Ryan W. Anderson <RyanWAnderson@users.noreply.github.com>
2016-09-21 09:27:12 -0500

    Update TimestampedTracingService.cs
    Removed dependency on extensions.
    Added Sample Code disclaimer.

--- Projects/Plugins/TimestampedTracingService.cs
+++ Projects/Plugins/TimestampedTracingService.cs
@@ -1,62 +1,71 @@
-﻿using System;
+// =====================================================================
+//  This file is part of the Microsoft CRM SDK Code Samples.
+//
+//  Copyright (C) Microsoft Corporation.  All rights reserved.
+//
+//  This source code is intended only as a supplement to Microsoft
+//  Development Tools and/or on-line documentation.  See these other
+//  materials for detailed information regarding Microsoft code samples.
+//
+//  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+//  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//  PARTICULAR PURPOSE.
+// =====================================================================
+
 using Microsoft.Xrm.Sdk;
 
-using Crm.CommunitySupport.Extensions;
-
-namespace Crm.CommunitySupport.Plugins {
+namespace Microsoft.Crm.Sdk.Samples {
     /// <summary>
-    /// An implementation of ITracingService that prefixes all traced messages with a timestamp and deltas for diagnoising plugin performance/errors.
-    /// OOB tracing service usage:
-    ///   ITracingService trc = (ITracingService) serviceProvider.GetService(typeof(ITracingService));
+    /// An implementation of ITracingService that prefixes all traced messages with a timestamp and time deltas for diagnoising plugin performance issues.
+    /// Out-of-box tracing service usage:
+    ///    ITracingService trc = (ITracingService) serviceProvider.GetService(typeof(ITracingService));
     /// TimestampedTracingService usage:
-    ///   ITracingService trc = new TimestampedTracingService(serviceProvider);
+    ///    ITracingService trc = new TimestampedTracingService(serviceProvider);
     /// </summary>
-    public partial class TimestampedTracingService : ITracingService {
-        #region Constructor(s)
-        public TimestampedTracingService(IServiceProvider serviceProvider, DateTime? initialTimestamp = null) {
-            // Ensure the inititalTimestamp is not in the future (since servers may not be in sync)
-            DateTime utcNow = DateTime.UtcNow;
-            
-            initialTimestamp = (initialTimestamp.HasValue && initialTimestamp < utcNow) ? initialTimestamp.Value : utcNow;
+    class TimestampedTracingService : ITracingService {
+        /// <summary>
+        /// Create a new TimestampedTracingService relying on Xrm services from the IServiceProvider.
+        /// </summary>
+        /// <param name="serviceProvider">The IServiceProvider passed into a plugin's Execute() method.</param>
+        public TimestampedTracingService(IServiceProvider serviceProvider) {
+            var utcNow = DateTime.UtcNow;
+
+            // Get the initial timestamp from the IExecutionContext
+            var context = (IExecutionContext)serviceProvider.GetService( typeof( IExecutionContext ) );
+            var initialTimestamp = context.OperationCreatedOn;
+
+            // Ensure the inititalTimestamp is not in the future (since servers may not be exactly in sync)
+            if (initialTimestamp > utcNow) {
+                initialTimestamp = utcNow;
+            }
 
             // Set private members
-            _tracingService = serviceProvider.GetService<ITracingService>();
-            _firstTraceTime = _previousTraceTime = (initialTimestamp.HasValue ? initialTimestamp.Value : DateTime.UtcNow);
-            Trace("TimestampedTracingService initialized.");
+            _tracingService = (ITracingService)serviceProvider.GetService( typeof( ITracingService ) );
+            _firstTraceTime = _previousTraceTime = initialTimestamp;
+
+            // Trace a starting message
+            Trace( "TimestampedTracingService initialized." );
         }
-        #endregion
 
         #region ITracingService support
         /// <summary>
-        /// Trace a message prefixed with UTC timestamp, execution time and delta
+        /// Trace a formatted message prefixed with UTC timestamp, overall duration and delta since last trace
         /// </summary>
-        /// <param name="format"></param>
-        /// <param name="args"></param>
         public void Trace(string format, params object[] args) {
-            DateTime utcNow = DateTime.UtcNow;
+            var utcNow = DateTime.UtcNow;
 
-            TraceMessageWithTimestampAndDeltas(utcNow, format, args);
-            SaveTimestamp(utcNow);
+            _tracingService.Trace(
+                "[{0:O} - @{1:N0}ms (+{2:N0}ms)] - {3}",
+                utcNow,
+                (utcNow - _firstTraceTime).TotalMilliseconds,
+                (utcNow - _previousTraceTime).TotalMilliseconds,
+                string.Format( format, args )
+            );
+
+            _previousTraceTime = utcNow;
         }
         #endregion
-
-        /// <summary>
-        /// Trace the message, prefixed with the current time in UTC sortable format, with deltas
-        /// </summary>
-        private void TraceMessageWithTimestampAndDeltas(DateTime now, string format, object[] args) {
-            _tracingService.Trace(
-                string.Format(
-                    "[{0:O} - @{1:N0}ms (+{2:N0}ms)] - {3}",
-                    now,
-                    (now - _firstTraceTime).TotalMilliseconds,
-                    (now - _previousTraceTime).TotalMilliseconds,
-                    string.Format(format, args)
-                ));
-        }
-
-        private void SaveTimestamp(DateTime now) {
-            _previousTraceTime = now;
-        }
 
         // Base ITracingService
         private ITracingService _tracingService;
