90b74b6a827f989dd87c613f778404562a8961c5
Ryan Anderson (D365 SUPPORT) <Ryan.W.Anderson@microsoft.com>
2019-01-17 13:46:46 -0600

    New plugins to convert between savedquery and userquery.

---
+++ Projects/Plugins/Plugins/SaveAsSystemView.cs
@@ -0,0 +1,50 @@
+﻿namespace Crm.CommunitySupport.Plugins
+{
+    using Crm.CommunitySupport.Extensions;
+    using Microsoft.Xrm.Sdk;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// A plugin that saves a copy of user view as a system view.
+    /// </summary>
+    public class SaveAsSystemView : Plugin
+    {
+        private static readonly Dictionary<string, string> AttributeMap = new Dictionary<string, string>
+        {
+            { "advancedgroupby", "advancedgroupby" },
+            { "columnsetxml", "columnsetxml" },
+            { "conditionalformatting", "conditionalformatting" },
+            { "description", "description" },
+            { "fetchxml", "fetchxml" },
+            { "layoutjson", "layoutjson" },
+            { "layoutxml", "layoutxml" },
+            { "name", "name" },
+            { "offlinesqlquery", "offlinesqlquery" },
+            { "querytype", "querytype" },
+            { "returnedtypecode", "returnedtypecode" },
+            { "userqueryid", "savedqueryid" },
+        };
+
+        public override void ExecutePlugin(PluginExecutionContext _)
+        {
+            // Input
+            var userView = _.Retrieve(_.PrimaryEntityName, _.PrimaryEntityId,);
+            var systemView = ConvertToSystemView(userView);
+
+            // Output
+            _.OutputParameters["savedqueryid"] = _.Create(systemView);
+        }
+
+        private static Entity ConvertToSystemView(Entity userView)
+        {
+            var systemView = userView.MapAttributes(AttributeMap, "savedquery");
+
+            systemView["canbedeleted"] = true;
+            systemView["iscustomizable"] = true;
+            systemView["isdefault"] = false;
+            systemView["isquickfindquery"] = false;
+
+            return systemView;
+        }
+    }
+}
---
+++ Projects/Plugins/Plugins/SaveAsUserView.cs
@@ -0,0 +1,37 @@
+﻿using Crm.CommunitySupport.Extensions;
+using System.Collections.Generic;
+
+namespace Crm.CommunitySupport.Plugins
+{
+    /// <summary>
+    /// A plugin that saves a copy of a system view as a user view.
+    /// </summary>
+    public class SaveAsUserView : Plugin
+    {
+        private static readonly Dictionary<string, string> AttributeMap = new Dictionary<string, string>
+        {
+            { "advancedgroupby", "advancedgroupby" },
+            { "columnsetxml", "columnsetxml" },
+            { "conditionalformatting", "conditionalformatting" },
+            { "description", "description" },
+            { "fetchxml", "fetchxml" },
+            { "layoutjson", "layoutjson" },
+            { "layoutxml", "layoutxml" },
+            { "name", "name" },
+            { "offlinesqlquery", "offlinesqlquery" },
+            { "querytype", "querytype" },
+            { "returnedtypecode", "returnedtypecode" },
+            { "savedqueryid", "userqueryid" },
+        };
+
+        public override void ExecutePlugin(PluginExecutionContext _)
+        {
+            // Input
+            var systemView = _.Retrieve(_.PrimaryEntityName, _.PrimaryEntityId);
+            var userView = systemView.MapAttributes(AttributeMap, "userquery");
+
+            // Output
+            _.OutputParameters["userqueryid"] = _.Create(userView);
+        }
+    }
+}
